0d475d0d8965430c130c27098aaa4c755e59e6af
bapidec <bapidec@gmail.com>
2022-10-18 09:21:05 +0200

    Modified Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println(formatter.format(date));
 
     }
 
